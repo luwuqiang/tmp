c60ed35725c09fb043ab4b4dc649492d2f938295
liuwuqiang <lwq@golanghub.com>
2017-01-04 16:19:50 +0800

    +debug

--- 会议通知模块/short_application_approval.sql
+++ 会议通知模块/short_application_approval.sql
@@ -8,7 +8,8 @@ CREATE TABLE `t_mo_application` (
   `create_time` bigint(20) DEFAULT NULL COMMENT '创建时间',
   `update_time` bigint(20) DEFAULT NULL COMMENT '更新时间',
   `status` tinyint(4) DEFAULT '0' COMMENT '审批状态，0:待审批 1:审批中 2:不同意 3:已同意',
-  `photo_group_id` bigint(20) DEFAULT '0' COMMENT '图组id,关联t_sys_file_info的obj_id',
+  `photo_group_id` bigint(20) DEFAULT NULL COMMENT '图组id,关联t_sys_file_info的obj_id',
+  `state` tinyint(4) DEFAULT '1' COMMENT '0:已读,1:未读',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='移动办公申请表';
 
@@ -17,7 +18,7 @@ CREATE TABLE `t_mo_application_account` (
   `applic_id` bigint(20) DEFAULT NULL,
   `amount` double DEFAULT '0' COMMENT '总金额',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='财务申请表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='财务申请表';
 
 CREATE TABLE `t_mo_application_account_item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -25,12 +26,12 @@ CREATE TABLE `t_mo_application_account_item` (
   `amount` double DEFAULT '0',
   `detail_desc` varchar(300) DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='报销明细表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='报销明细表';
 
 CREATE TABLE `t_mo_application_leave` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `applic_id` bigint(20) NOT NULL COMMENT '关联t_mo_application ',
-  `leave_days` tinyint(4) DEFAULT NULL COMMENT '请假天数',
+  `leave_days` int(11) DEFAULT NULL COMMENT '请假天数',
   `begin_time` bigint(20) DEFAULT NULL COMMENT '开始时间',
   `end_time` bigint(20) DEFAULT NULL COMMENT '结束时间',
   PRIMARY KEY (`id`)
@@ -43,7 +44,7 @@ CREATE TABLE `t_mo_application_out` (
   `end_time` bigint(20) DEFAULT NULL COMMENT '结束时间',
   `take_hours` double DEFAULT NULL COMMENT '外出花费时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='外出申请表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='外出申请表';
 
 CREATE TABLE `t_mo_application_purchase` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -78,6 +79,8 @@ CREATE TABLE `t_mo_approval` (
   `status` tinyint(4) DEFAULT '0' COMMENT '用户审批结果 0:待审核 1:不同意 2:同意',
   `approval_time` bigint(20) DEFAULT NULL COMMENT '审核时间',
   `reason` varchar(300) DEFAULT NULL COMMENT '理由',
+  `state` tinyint(4) DEFAULT '1' COMMENT '0:已读,1:未读',
+  `msg_id` bigint(20) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='审批表';
 
@@ -98,7 +101,6 @@ CREATE TABLE `t_mo_approval_step_mold_item` (
   `step` tinyint(4) DEFAULT NULL COMMENT '使用1.2.3 ...来标记流程',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='审批流程模型明细项';
-
 
 INSERT INTO `t_mo_application_type` (`id`,`mold`,`name`,`seq`) VALUES (1,1,'事假',1);
 INSERT INTO `t_mo_application_type` (`id`,`mold`,`name`,`seq`) VALUES (2,1,'病假',2);
